b27f490a3737184095052babcbfdc37e7878c1e8
Mike Rovers <mikerovers97@gmail.com>
2018-08-13 00:54:30 +0200

    Update LICENSE

--- src/Providers/LaravelFiltersServiceProvider.php
+++ src/Providers/LaravelFiltersServiceProvider.php
@@ -3,7 +3,6 @@
 namespace MikeRovers\LaravelFilters\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Mikerovers\LaravelFilters\Commands\CreateFilterCollection;
 
 class LaravelFiltersServiceProvider extends ServiceProvider
 {
@@ -16,7 +15,7 @@ class LaravelFiltersServiceProvider extends ServiceProvider
     {
 		if ($this->app->runningInConsole()) {
 			$this->commands([
-				CreateFilterCollection::class
+				
 			]);
 		}
     }
